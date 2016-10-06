f50d37f2a6b7aae0f5a48f0b26cb48bef978abbc
Tomas <tomasg88@gmail.com>
2016-10-06 10:54:42 -0300

    agrego un modelo y chau desde la bangho

--- app/routes/news.js
+++ app/routes/news.js
@@ -1,30 +1,25 @@
 import Ember from 'ember';
 
 let news = [{
-  id: 0,
   title: 'Grand Old Mansion',
   author: 'Veruca Salt',
   text: 'San Francisco',
-  publishDate: 'Estate',
   image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg'
 }, {
-  id: 1,
   title: 'Urban Living',
   author: 'Mike TV',
   text: 'Seattle',
-  publishDate: 'Condo',
   image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg'
 }, {
-  id: 2,
   title: 'Downtown Charm',
   author: 'Violet Beauregarde',
   text: 'Portland',
-  publishDate: 'Apartment',
   image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg'
 }];
 
 export default Ember.Route.extend({
 	model(){
-		return news;
+		//return this.get('store').findAll('news');
+    return news;
 	}
 });
